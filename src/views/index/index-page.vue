93947ace7d59552c53330a55d0043f2378d3d512
zhangcheng <2778162192@qq.com>
2022-05-27 10:15:40 +0800

    commit update

---
+++ src/views/index/index-page.vue
@@ -0,0 +1,18 @@
+<template>
+    <div>
+        <h1>This is Mr.Orange's Page</h1>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "index-page",
+    data() {
+        return {}
+    }
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
